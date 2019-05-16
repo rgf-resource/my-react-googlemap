5d13ef3a51391db92a18eae9d9c8d5b62a5c94c5
Ryan Ferris <ryanglenferris@gmail.com>
2019-05-16 06:40:35 -0700

    .gitignore
    tells git to ignore all .env files.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,28 @@ const mapStyles = {
 };
 
 export class MapContainer extends Component {
+	state = {
+		showingInfoWindow: false, //Hides or the shows the infoWindow
+		activeMarker: {}, //Shows the active marker upon click
+		selectedPlace: {}, //Shows the infoWindow to the selected place upon a marker
+	};
+
+	onMarkerClick = (props, marker, e) =>
+		this.setState({
+			selectedPlace: props,
+			activeMarker: marker,
+			showingInfoWindow: true,
+		});
+
+	onClose = props => {
+		if (this.state.showingInfoWindow) {
+			this.setState({
+				showingInfoWindow: false,
+				activeMarker: null,
+			});
+		}
+	};
+
 	render() {
 		return (
 			<Map
